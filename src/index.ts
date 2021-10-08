7d4089d056d2a8bea5737a8e4172cfa6478498ce
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-10-08 19:05:56 +0800

    implemented and tested async APIs compress() and decompress()

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { spawnSync } from 'child_process'
+import { spawn, spawnSync } from 'child_process'
 
 export type CompressSyncOptions = {
   compressLevel?: number // 1 to 19, default is 3
@@ -23,11 +23,60 @@ export function compressSync(options: CompressSyncOptions): Buffer {
   return result.stdout
 }
 
-export type DeCompressSyncOptions = {
+export type CompressOptions = CompressSyncOptions
+
+export function compress(options: CompressOptions): Promise<Buffer>
+export function compress(
+  options: CompressOptions,
+  callback: (error: any, buffer: Buffer) => void,
+): void
+export function compress(
+  options: CompressOptions,
+  maybeCallback?: (error: any, buffer: Buffer) => void,
+): Promise<Buffer> | void {
+  if (!maybeCallback) {
+    return new Promise((resolve, reject) =>
+      compress(options, (error, result) =>
+        error ? reject(error) : resolve(result),
+      ),
+    )
+  }
+  const callback = maybeCallback
+  const cmd = 'zstd'
+  const args: string[] = []
+  if ('compressLevel' in options) {
+    args.push('-' + options.compressLevel)
+  }
+  const child = spawn(cmd, args)
+  const buffer: Buffer[] = []
+  let totalLength = 0
+  let hasError = false
+  child.stdout
+    .on('data', chunk => {
+      if (hasError) return
+      buffer.push(chunk)
+      totalLength += chunk.length
+    })
+    .on('error', error => {
+      if (hasError) return
+      hasError = true
+      callback(error, null as any)
+    })
+    .on('end', () => {
+      if (!hasError) callback(null, Buffer.concat(buffer, totalLength))
+    })
+  child.stdin.write(options.input)
+  child.stdin.end()
+}
+
+export type DecompressSyncOptions = {
   input: Buffer
 }
 
-export function decompressSync(options: DeCompressSyncOptions): Buffer {
+/** @deprecated renamed to DecompressSyncOptions */
+export type DeCompressSyncOptions = DecompressSyncOptions
+
+export function decompressSync(options: DecompressSyncOptions): Buffer {
   const result = spawnSync('unzstd', {
     input: options.input,
   })
@@ -38,4 +87,45 @@ export function decompressSync(options: DeCompressSyncOptions): Buffer {
     throw new Error(result.stderr.toString())
   }
   return result.stdout
+}
+
+export type DecompressOptions = DecompressSyncOptions
+
+export function decompress(options: DecompressSyncOptions): Promise<Buffer>
+export function decompress(
+  options: DecompressSyncOptions,
+  callback: (error: any, buffer: Buffer) => void,
+): void
+export function decompress(
+  options: DecompressSyncOptions,
+  maybeCallback?: (error: any, buffer: Buffer) => void,
+): Promise<Buffer> | void {
+  if (!maybeCallback) {
+    return new Promise((resolve, reject) =>
+      decompress(options, (error, result) =>
+        error ? reject(error) : resolve(result),
+      ),
+    )
+  }
+  const callback = maybeCallback
+  const child = spawn('unzstd')
+  const buffer: Buffer[] = []
+  let totalLength = 0
+  let hasError = false
+  child.stdout
+    .on('data', chunk => {
+      if (hasError) return
+      buffer.push(chunk)
+      totalLength += chunk.length
+    })
+    .on('error', error => {
+      if (hasError) return
+      hasError = true
+      callback(error, null as any)
+    })
+    .on('end', () => {
+      if (!hasError) callback(null, Buffer.concat(buffer, totalLength))
+    })
+  child.stdin.write(options.input)
+  child.stdin.end()
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,40 +1,146 @@
 import { readFileSync, unlinkSync } from 'fs'
 import { expect } from 'chai'
-import { compressSync, decompressSync } from '../src/index'
+import {
+  compress,
+  compressSync,
+  decompress,
+  decompressSync,
+} from '../src/index'
 import { execSync } from 'child_process'
 
 describe('zstd.ts core TestSuit', () => {
-  context('test with default compress level', () => {
-    let actualCompressed: Buffer
-    it('should compress with default compress level', () => {
-      execSync('echo -n test | zstd > raw')
-      let expectedCompressed = readFileSync('raw')
-      actualCompressed = compressSync({ input: 'test' })
-      expect(actualCompressed).to.deep.equals(expectedCompressed)
-      unlinkSync('raw')
-    })
-    it('should decompress from default compress level', () => {
-      let decompressed = decompressSync({ input: actualCompressed })
-      expect(decompressed.toString()).to.equals('test')
-    })
-  })
-
-  context('test with 1-19 compress level', () => {
+  let expectedDefaultCompressed: Buffer
+  let expectedLeveledCompressed: Buffer[]
+  before(() => {
+    execSync('echo -n test | zstd > raw')
+    expectedDefaultCompressed = readFileSync('raw')
+    expectedLeveledCompressed = []
     for (let i = 1; i <= 19; i++) {
-      let actualCompressed: Buffer
-      it('should compress with compress level ' + i, () => {
-        execSync(`echo -n test | zstd -${i} > raw`)
-        let expectedCompressed = readFileSync('raw')
-        actualCompressed = compressSync({ input: 'test', compressLevel: i })
-        expect(actualCompressed).to.deep.equals(expectedCompressed)
+      execSync(`echo -n test | zstd -${i} > raw`)
+      expectedLeveledCompressed[i] = readFileSync('raw')
+    }
+    unlinkSync('raw')
+  })
+  context('sync API', () => {
+    context('test with default compress level', () => {
+      it('should compress with default compress level', () => {
+        let actualCompressed = compressSync({ input: 'test' })
+        expect(actualCompressed).to.deep.equals(expectedDefaultCompressed)
       })
-      it('should decompress with compress level ' + i, () => {
-        let decompressed = decompressSync({ input: actualCompressed })
+      it('should decompress from default compress level', () => {
+        let decompressed = decompressSync({ input: expectedDefaultCompressed })
         expect(decompressed.toString()).to.equals('test')
       })
-    }
-    after(() => {
-      unlinkSync('raw')
+    })
+
+    context('test with 1-19 compress level', () => {
+      for (let i = 1; i <= 19; i++) {
+        it('should compress with compress level ' + i, () => {
+          let actualCompressed = compressSync({
+            input: 'test',
+            compressLevel: i,
+          })
+          expect(actualCompressed).to.deep.equals(expectedLeveledCompressed[i])
+        })
+        it('should decompress with compress level ' + i, () => {
+          let decompressed = decompressSync({
+            input: expectedLeveledCompressed[i],
+          })
+          expect(decompressed.toString()).to.equals('test')
+        })
+      }
+    })
+  })
+  context('async API', () => {
+    context('async function overload', () => {
+      context('compress()', () => {
+        it('should invoke callback if given', done => {
+          compress({ input: 'test' }, (error, actualCompressed) => {
+            expect(error).to.be.null
+            expect(actualCompressed).to.deep.equals(expectedDefaultCompressed)
+            done()
+          })
+        })
+        it('should return promise if not given callback', done => {
+          let promise = compress({ input: 'test' })
+          expect(promise).not.undefined
+          expect(promise.then).to.be.a('function')
+          promise.then(actualCompressed => {
+            expect(actualCompressed).to.deep.equals(expectedDefaultCompressed)
+            done()
+          })
+        })
+      })
+      context('decompress()', () => {
+        it('should invoke callback if given', done => {
+          decompress(
+            { input: expectedDefaultCompressed },
+            (error, actualDecompressed) => {
+              expect(error).to.be.null
+              expect(actualDecompressed.toString()).to.equals('test')
+              done()
+            },
+          )
+        })
+        it('should return promise if not given callback', done => {
+          let promise = decompress({ input: expectedDefaultCompressed })
+          expect(promise).not.undefined
+          expect(promise.then).to.be.a('function')
+          promise.then(actualDecompressed => {
+            expect(actualDecompressed.toString()).to.equals('test')
+            done()
+          })
+        })
+      })
+    })
+    context('test with default compress level', () => {
+      let actualCompressed: Buffer
+      it('should compress with default compress level', done => {
+        compress({ input: 'test' }, (error, result) => {
+          expect(error).to.be.null
+          expect(result).to.deep.equals(expectedDefaultCompressed)
+          actualCompressed = result
+          done()
+        })
+      })
+      it('should decompress from default compress level', done => {
+        decompress({ input: actualCompressed }, (error, decompressed) => {
+          expect(error).to.be.null
+          expect(decompressed.toString()).to.equals('test')
+          done()
+        })
+      })
+    })
+    context('test with 1-19 compress level', () => {
+      for (let i = 1; i <= 19; i++) {
+        it('should compress with compress level ' + i, done => {
+          compress(
+            {
+              input: 'test',
+              compressLevel: i,
+            },
+            (error, actualCompressed) => {
+              expect(error).to.be.null
+              expect(actualCompressed).to.deep.equals(
+                expectedLeveledCompressed[i],
+              )
+              done()
+            },
+          )
+        })
+        it('should decompress with compress level ' + i, done => {
+          decompress(
+            {
+              input: expectedLeveledCompressed[i],
+            },
+            (error, decompressed) => {
+              expect(error).to.be.null
+              expect(decompressed.toString()).to.equals('test')
+              done()
+            },
+          )
+        })
+      }
     })
   })
 })
